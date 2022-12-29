6f990cdd2b599167ea33d3b473f4d3ec3c4460a6
Kristian Quirapas <kmquirapas@gmail.com>
2022-12-29 11:12:58 +0800

    add: favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -159,7 +159,24 @@ export default function Page() {
           content="https://bafybeiajggm7vcbon5n57p27qqmnxs2gptprgd5lvnewlv4zkv5hxsu2qi.ipfs.nftstorage.link/"
         />
         <meta property="og:type" content="website" />
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       {/*<Container styling={showMobileNav ? "overflow-hidden" : ""}>*/}
       {/* NAVBAR */}
