ab29b8dd442940628f6395618277606bfd26d552
Lyzer Merck Bautista <66999325+mercklyzer@users.noreply.github.com>
2023-04-01 22:27:55 +0800

    Replace landing page with about

--- modules/common/Mission.tsx
+++ modules/common/Mission.tsx
@@ -12,19 +12,19 @@ const Mission: FunctionComponent = () => {
                 backgroundSize: 'cover',
                 backgroundPosition: 'center'
             }}>
-            <div className='w-full max-w-[1200px] flex flex-col 
-                items-center lg:items-start 
-                justify-center 
+            <div className='w-full max-w-[1200px] flex flex-col
+                items-center lg:items-start
+                justify-center
                 space-y-8 lg:space-y-12
                 px-4 sm:px-8 md:px-16'>
-                <h1 className='font-montserrat-bold text-white 
+                <h1 className='font-montserrat-bold text-white
                     text-4xl lg:text-6xl'>Our Mission</h1>
-                <p className='text-white font-montserrat-regular  
-                    
+                <p className='text-white font-montserrat-regular
+
                     max-w-[90%] lg:max-w-[50%]
                     text-center lg:text-left
                     text-xl lg:text-2xl'>
-                    We're on a mission to build the future of the Metaverse powered by the Creator Industry. 
+                    We're on a mission to build the future of the Metaverse powered by the Creator Industry.
                 </p>
 
             </div>
